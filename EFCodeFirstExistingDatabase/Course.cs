baef01535b828bbe853c7e2c6a05fd182f266b9b
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-12-20 13:18:23 +0530

    Created migration DeleteDatePublishedColumnFromCoursesTable and updated the database

--- EFCodeFirstExistingDatabase/Course.cs
+++ EFCodeFirstExistingDatabase/Course.cs
@@ -22,7 +22,6 @@ namespace EFCodeFirstExistingDatabase
         public string Description { get; set; }
 
         public Category Category { get; set; }
-        public DateTime? DatePublished { get; set; }
 
         public int Level { get; set; }
 
