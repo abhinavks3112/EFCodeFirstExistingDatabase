d23e4ae279ddcf88ba5a79ef1d0df4655089f759
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-12-21 13:53:18 +0530

    Deleted Category Column from Courses table, created a backup/history _Categories table for preserving Categories Data, created migration for both and updated the database

---
+++ EFCodeFirstExistingDatabase/Migrations/201912210812220_DeleteCategoriesTable.cs
@@ -0,0 +1,43 @@
+namespace EFCodeFirstExistingDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DeleteCategoriesTable : DbMigration
+    {
+        public override void Up()
+        {
+            // Creating a backup/history table
+            CreateTable(
+               "dbo._Categories",
+               c => new
+               {
+                   Id = c.Int(nullable: false, identity: true),
+                   Name = c.String(),
+               })
+               .PrimaryKey(t => t.Id);
+
+            Sql("INSERT INTO _Categories (Name) SELECT Name FROM Categories");
+
+            DropTable("dbo.Categories");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+
+            // Copying from backup/history table
+            Sql("INSERT INTO Categories (Name) SELECT Name FROM _Categories");
+
+            // Dropping the backup/history table
+            DropTable("dbo._Categories");
+        }
+    }
+}
